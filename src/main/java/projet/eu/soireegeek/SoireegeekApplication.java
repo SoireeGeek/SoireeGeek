5b3455ee86b8097662b9119a0cbb1546057f0ebe
Oldelaf <72396968+Oldelaf@users.noreply.github.com>
2022-05-19 22:35:00 +0200

    first commit

---
+++ src/main/java/projet/eu/soireegeek/SoireegeekApplication.java
@@ -0,0 +1,13 @@
+package projet.eu.soireegeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoireegeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoireegeekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projet/eu/soireegeek/SoireegeekApplicationTests.java
@@ -0,0 +1,8 @@
+package projet.eu.soireegeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoireegeekApplicationTests {
+}
